fa47e5ccd928ac2933a0bf26e3e40a1d5817daa4
Stephanie Dover <doverstephaniem@gmail.com>
2017-08-31 14:07:09 -0700

    refactoring code

--- src/lib/store-create.js
+++
@@ -1,7 +0,0 @@
-import thunk from './redux-thunk.js'
-import reporter from './redux-reporter.js'
-import { persistStore, autoRehydrate } from 'redux-persist'
-import { compose, createStore, applyMiddleware } from 'redux'
-
-export default () =>
-  createStore(compose(applyMiddleware(thunk, reporter), autoRehydrate()))
